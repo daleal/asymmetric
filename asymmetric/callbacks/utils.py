f935896a0e722aad034b1a0a4e89d938ccff1de2
Daniel Leal <dlleal@uc.cl>
2020-10-23 15:57:18 -0300

    Annotate utils of callbacks module

--- asymmetric/callbacks/utils.py
+++ asymmetric/callbacks/utils.py
@@ -2,11 +2,13 @@
 A module for asymmetric's callback utilities.
 """
 
+from typing import Any, Dict, Union
+
 from asymmetric.callbacks.callback_object import CALLBACK_OBJECT_SCHEMA
 from asymmetric.utils import valid_plain_dict
 
 
-def valid_callback_data(callback):
+def valid_callback_data(callback: Union[Dict[str, Any], bool]) -> bool:
     """
     Returns a boolean indicating if the callback object is a valid json
     conforming to the json schema.
