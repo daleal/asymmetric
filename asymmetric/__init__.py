bc9d5d183509660f687388e9d05043018290ea66
Daniel Leal <37158127+daleal@users.noreply.github.com>
2020-10-24 00:26:19 -0300

    Fix and release (#10)
    * Fix output command on workflow
    
    * Update versions

--- asymmetric/__init__.py
+++ asymmetric/__init__.py
@@ -4,5 +4,5 @@ Init file for the asymmetric module.
 
 from asymmetric.core import asymmetric_object as asymmetric
 
-version_info = (0, 1, 3)
+version_info = (0, 1, 4)
 __version__ = ".".join([str(x) for x in version_info])
