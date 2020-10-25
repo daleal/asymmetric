a11a1f3ccdf78e8511d0a72368966a49e69e6ca6
Daniel Leal <dlleal@uc.cl>
2020-10-25 05:35:55 -0300

    Bump versions up

--- asymmetric/__init__.py
+++ asymmetric/__init__.py
@@ -4,5 +4,5 @@ Init file for the asymmetric module.
 
 from asymmetric.core import asymmetric_object as asymmetric
 
-version_info = (0, 1, 6)
+version_info = (0, 2, 0)
 __version__ = ".".join([str(x) for x in version_info])
