cbc1f3d5ac7ace437010b087c5440844df6bb209
Daniel Leal <37158127+daleal@users.noreply.github.com>
2020-10-27 21:51:54 -0300

    Prepare 0.3.0 release (#35)

--- asymmetric/__init__.py
+++ asymmetric/__init__.py
@@ -4,5 +4,5 @@ Init file for the asymmetric module.
 
 from asymmetric.core import asymmetric_object as asymmetric
 
-version_info = (0, 2, 1)
+version_info = (0, 3, 0)
 __version__ = ".".join([str(x) for x in version_info])
